6a23c9a437ba680c30523a58bf7078ab51619575
Baptiste Ory <orybaptiste@gmail.com>
2020-05-21 11:55:14 +0200

    build: Initialization of repository

---
+++ js/sketch.js
@@ -0,0 +1,28 @@
+/*------------------------------
+General
+------------------------------*/
+"use strict"; //Force to declare any variable used
+
+//const game = document.getElementById("game");
+
+/*------------------------------
+Initialisation
+------------------------------*/
+document.addEventListener("DOMContentLoaded", initialiser);
+
+function initialiser(evt) {
+    
+}
+
+function setup() {
+    var canvas = createCanvas(400, 400);
+    canvas.parent("canvasHolder");
+}
+
+function draw() {
+  background(220);
+}
+
+/*------------------------------
+Game
+------------------------------*/
