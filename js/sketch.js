dd27b172be0f8545e0c5740a37c827dc37f6ccb9
Solène <45456425+SolHaine@users.noreply.github.com>
2020-05-22 17:27:50 +0200

    max five passersby with linear movement and same starting point

--- js/sketch.js
+++ js/sketch.js
@@ -3,6 +3,8 @@ General
 ------------------------------*/
 "use strict"; //Force to declare any variable used
 
+let passersby = [[[-100, -100], [0, 0]]]
+
 //const game = document.getElementById("game");
 
 /*------------------------------
@@ -21,8 +23,30 @@ function setup() {
 
 function draw() {
   background(220);
+  movement();
 }
 
 /*------------------------------
 Game
 ------------------------------*/
+
+function movement() {
+  for(let i=0; i<passersby.length; i++) {
+    if(passersby[i][1][0] === 0 && passersby[i][1][1] === 0) {
+      passersby[i][1][0] = random(0.5, 5);
+      passersby[i][1][1] = random(0, 5);
+    }
+    passersby[i][0][0] += passersby[i][1][0];
+    passersby[i][0][1] += passersby[i][1][1];
+    fill(0)
+    ellipse(passersby[i][0][0], passersby[i][0][1], 100, 100)
+    if(passersby[i][0][0] < -100 || passersby[i][0][0] > width+100 || passersby[i][0][1] < -100 || passersby[i][0][1] > height+100) {
+      passersby[i] = passersby.pop()
+      
+    }
+  }
+  if(passersby.length < 5 && random(0, 100) > 98) {
+    passersby.push([[-100, -100], [0, 0]])
+  }
+}
+
